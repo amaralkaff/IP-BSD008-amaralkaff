ac1fd0a5a1f2cba4383d1918b9b54544048dc451
amaralkaff <tpamangly@gmail.com>
2023-11-17 02:46:54 +0700

    feat: add vercel.json

--- client/Social-Media-App/src/pages/PostPage.jsx
+++ client/Social-Media-App/src/pages/PostPage.jsx
@@ -44,47 +44,46 @@ const PostPage = () => {
     setPost({ ...post, Comments: [...post.Comments, newComment] });
   };
 
-  const handleCommentDelete = async (commentId) => {
-    try {
-      // Assuming an API endpoint for comment deletion
-      await axios.delete(`http://localhost:3000/comments/${commentId}`);
-      setPost({
-        ...post,
-        Comments: post.Comments.filter((c) => c.id !== commentId),
-      });
-    } catch (error) {
-      console.error("Error deleting comment:", error);
-    }
+  const handleCommentDeleted = (commentId) => {
+    setPost({
+      ...post,
+      Comments: post.Comments.filter((comment) => comment.id !== commentId),
+    });
   };
-
-  if (!post) return <div className="container mx-auto p-4">Loading...</div>;
 
   return (
     <div className="container mx-auto p-4">
       <div className="flex justify-center items-center">
         <div className="w-full max-w-md">
           <div className="bg-white shadow-lg rounded px-12 pt-6 pb-8 mb-4">
-            <div className="flex justify-between items-center mb-4">
-              <div className="flex items-center">
-                <img
-                  className="w-12 h-12 rounded-full mr-4"
-                  src={profile_picture}
-                  alt={name}
-                />
-                <p className="text-gray-900 font-semibold">{name}</p>
-              </div>
-              <p className="text-xs text-gray-500">
-                {new Date(post.createdAt).toLocaleString()}
-              </p>
+            <div className="flex justify-center items-center">
+              <img
+                className="w-24 h-24 rounded-full mx-auto"
+                src={profile_picture}
+                alt={name}
+              />
             </div>
-            <p className="text-gray-700 text-base">{post.content}</p>
+            <div className="text-center mt-4">
+              <h1 className="block text-gray-700 font-bold mb-2 text-xl">
+                {name}
+              </h1>
+              <p className="text-gray-600">{bio}</p>
+            </div>
           </div>
-          <CommentForm postId={post.id} onCommentAdded={handleCommentAdded} />
-          {/* <CommentList
-            postId={post.id}
-            comments={post.Comments}
-            onDelete={handleCommentDelete}
-          /> */}
+          <div className="bg-white shadow-lg rounded px-12 pt-6 pb-8 mb-4">
+            <h1 className="block text-gray-700 font-bold mb-2 text-xl text-center">
+              {post?.title}
+            </h1>
+            <p className="text-gray-600">{post?.content}</p>
+          </div>
+          <CommentForm postId={id} onCommentAdded={handleCommentAdded} />
+          <div className="bg-white shadow-lg rounded px-12 pt-6 pb-8 mb-4">
+            <Comment
+              comment={post?.Comments}
+              onDelete={handleCommentDeleted}
+              postId={id}
+            />
+          </div>
         </div>
       </div>
     </div>
