354f8d939179d7c8cb6c6e0251e41a5b812c04a0
amaralkaff <tpamangly@gmail.com>
2023-11-17 02:39:27 +0700

    feat: deployment

--- client/Social-Media-App/src/components/comment.jsx
+++ client/Social-Media-App/src/components/comment.jsx
@@ -1,28 +1,45 @@
 // components/comment.jsx
-import React, { useCallback } from "react";
-import Comment from "../components/Comment";
+import { useCallback } from "react";
+import propTypes from "prop-types";
 
-const Comment = ({ comment, onDelete }) => {
-  const handleDelete = useCallback(() => {
+let Comment = ({ comment, onDelete }) => {
+  let handleDelete = useCallback(() => {
     onDelete(comment.id);
   }, [comment.id, onDelete]);
 
   return (
-    <div className="border-b border-gray-300 mb-2 pb-2">
-      <p className="font-semibold">{comment.User?.username}</p>
-      <p>{comment.content}</p>
-      {/* Add more comment details */}
-      <div className="flex items-center space-x-2">
-        <p className="text-xs text-gray-500">
-          {new Date(comment.createdAt).toLocaleString()}
-        </p>
-        {/* Add delete button */}
-        <button className="text-xs text-red-500" onClick={handleDelete}>
-          Delete
-        </button>
+    <div className="flex space-x-2">
+      <img
+        src={comment.User.profile_picture}
+        alt={`${comment.User.name}'s profile`}
+        className="w-8 h-8 rounded-full"
+      />
+      <div className="flex-1 space-y-1">
+        <div className="flex items-center justify-between">
+          <p className="font-semibold">{comment.User.name}</p>
+          <button
+            className="text-xs text-gray-400 hover:text-gray-600"
+            onClick={handleDelete}
+          >
+            Delete
+          </button>
+        </div>
+        <p className="text-sm">{comment.content}</p>
       </div>
     </div>
   );
+};
+
+Comment.propTypes = {
+  comment: propTypes.shape({
+    id: propTypes.number.isRequired,
+    content: propTypes.string.isRequired,
+    User: propTypes.shape({
+      name: propTypes.string.isRequired,
+      profile_picture: propTypes.string.isRequired,
+    }),
+  }),
+  onDelete: propTypes.func.isRequired,
 };
 
 export default Comment;
